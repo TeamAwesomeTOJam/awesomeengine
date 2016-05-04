b078fe6dde5a7afeb9ff3b12368a3956cb862af5
Daniel Lister <dan.lister@gmail.com>
2016-05-03 20:40:38 -0400

    scaling fixes for weird zooms

--- awesomeengine/camera.py
+++ awesomeengine/camera.py
@@ -17,7 +17,8 @@ class Camera(object):
 
         self.transform_point = self.world_to_camera
         self.transform_angle = self.world_to_camera_angle
-        self.transform_length = self.world_to_camera_length
+        self.transform_width = self.world_to_camera_width
+        self.transform_height = self.world_to_camera_height
 
     def render(self):
 
@@ -27,14 +28,16 @@ class Camera(object):
 
         self.transform_point = self.world_to_camera
         self.transform_angle = self.world_to_camera_angle
-        self.transform_length = self.world_to_camera_length
+        self.transform_width = self.world_to_camera_width
+        self.transform_height = self.world_to_camera_height
 
         for layer in self.layers:
             layer.draw(self)
 
         self.transform_point = self.hud_to_camera
         self.transform_angle = self.hud_to_camera_angle
-        self.transform_length = self.hud_to_camera_length
+        self.transform_width = self.hud_to_camera_length
+        self.transform_height = self.hud_to_camera_height
 
         for e in self.hud_entities:
             e.handle('draw', self)
@@ -54,8 +57,11 @@ class Camera(object):
     def hud_to_camera_angle(self, a):
         return -a
 
-    def hud_to_camera_length(self, l):
-        return l
+    def hud_to_camera_length(self, w):
+        return w
+
+    def hud_to_camera_height(self, h):
+        return h
 
 
     def world_to_camera(self, p):
@@ -68,16 +74,19 @@ class Camera(object):
         cx = rx + self.entity.width / 2
         cy = ry + self.entity.height / 2
 
-        sx = float(self.entity.screen_width) / self.entity.width * cx
-        sy = float(self.entity.screen_height) / self.entity.height * cy
+        sx = self.transform_width(cx)
+        sy = self.transform_height(cy)
 
         return int(sx), int(self.entity.screen_height - sy)
 
     def world_to_camera_angle(self, a):
         return self.entity.angle - a
 
-    def world_to_camera_length(self, l):
-        return int(float(self.entity.screen_width)/self.entity.width*l)
+    def world_to_camera_width(self, w):
+        return int(float(self.entity.screen_width)/self.entity.width*w)
+
+    def world_to_camera_height(self, h):
+        return int(float(self.entity.screen_height) / self.entity.height * h)
 
     def draw_rect(self, c, r):
         points = [r.top_left, r.bottom_left, r.bottom_right, r.top_right, r.top_left]
@@ -88,7 +97,7 @@ class Camera(object):
 
     def draw_image(self, r, texture):
         x,y = self.transform_point(r.center)
-        w,h = self.transform_length(r.w), self.transform_length(r.h)
+        w,h = self.transform_width(r.w), self.transform_height(r.h)
         self.renderer.copy(texture, dest_rect=sdl2hl.Rect(int(x - w/2), int(y - h/2), w,h), rotation=self.transform_angle(r.a))
 
     def clear(self, colour):
