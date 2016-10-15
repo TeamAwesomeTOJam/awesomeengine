f4f751fa7395f9b8dcfd11a96279de4ab8df1865
Jonathan Doda <jonathan@jdoda.ca>
2016-10-15 13:50:08 -0400

    Reduce boilerplate for components.

--- awesomeengine/component.py
+++ awesomeengine/component.py
@@ -1,26 +1,30 @@
-from abc import ABCMeta, abstractmethod
+from abc import ABCMeta
 
 
 class Component:
     __metaclass__ = ABCMeta
 
-    @abstractmethod
     def add(self, entity):
-        pass
+        self.verify_attrs(entity)
+    
+        for event, handler in self.event_handlers:
+            entity.register_handler(event, handler)
 
-    @abstractmethod
     def remove(self, entity):
-        pass
-
-def verify_attrs(entity, attrs):
-    missing_attrs = []
-    for attr in attrs:
-        if isinstance(attr, tuple):
-            attr, default = attr
-            if not hasattr(entity, attr):
-                setattr(entity, attr, default)
-        else:
-            if not hasattr(entity, attr):
-                missing_attrs.append(attr)
-    if len(missing_attrs) > 0:
-        raise AttributeError("entity [%s] is missing required attributes [%s]" % (entity._static_data_name, missing_attrs))
+        for event, handler in self.event_handlers:
+            entity.unregister_handler(event, handler)
+        
+    def verify_attrs(self, entity):
+        missing_attrs = []
+        for attr in self.required_attrs:
+            if isinstance(attr, tuple):
+                attr, default = attr
+                if not hasattr(entity, attr):
+                    setattr(entity, attr, default)
+            else:
+                if not hasattr(entity, attr):
+                    missing_attrs.append(attr)
+        if len(missing_attrs) > 0:
+            raise AttributeError(
+                "entity [%s] is missing required attributes [%s]" 
+                % (entity._static_data_name, missing_attrs))
